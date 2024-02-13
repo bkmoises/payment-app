c3809f7d78102c4f3481f71fa0fb49fab258d356
bkmoises <bk_moises@hotmail.com>
2024-02-13 14:11:33 -0300

    chore: criação do app e das rotas raiz e user

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+// const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.status(200).json({ message: "Hello World!" });
+});
+
+app.post("/user", (req, res) => {
+  const user = req.body;
+  return res.status(201).json(user);
+});
+
+app.get("/user", (_req, res) => {
+  const user = [
+    {
+      userId: "1",
+      name: "user-1",
+      cpf: "123.456.789-10",
+      mail: "mail@mail.com",
+      passwd: "12345",
+      seller: true,
+    },
+  ];
+
+  return res.status(200).json(user);
+});
+
+app.put("/user/:id", (req, res) => {
+  const user = {
+    userId: "1",
+    name: "user-1",
+    cpf: "123.456.789-10",
+    mail: "mail@mail.com",
+    passwd: "12345",
+    seller: true,
+  };
+
+  user.name = req.body.name;
+  user.passwd = req.body.passwd;
+
+  return res.status(200).json(user);
+});
+
+module.exports = app;
