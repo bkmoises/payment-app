310f58f8fc12b1571fd7a5a8f954ee7662f07719
bkmoises <bk_moises@hotmail.com>
2024-02-13 18:08:26 -0300

    chore: Inclusão da documentação das rotas de usuário

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,15 +1,17 @@
 const router = require("express").Router();
 const userController = require("../controllers/user");
 
+// Rotas para manipulação de usuários
 router
   .route("/")
-  .post(userController.createUser)
-  .get(userController.getAllUsers);
+  .post(userController.createUser) // Criar um novo usuário
+  .get(userController.getAllUsers); // Obter todos os usuários
 
+// Rotas para manipulação de usuários por ID
 router
   .route("/:id")
-  .get(userController.getUserById)
-  .put(userController.updateOneUser)
-  .delete(userController.deleteOneUser);
+  .get(userController.getUserById) // Obter um usuário pelo ID
+  .put(userController.updateOneUser) // Atualizar um usuário pelo ID
+  .delete(userController.deleteOneUser); // Excluir um usuário pelo ID
 
 module.exports = router;
