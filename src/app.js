cdf912dfaf26fd975166714b83578bcb31502f79
bkmoises <bk_moises@hotmail.com>
2024-02-13 14:28:26 -0300

    chore: Inclusão do conector de bando de dados mongoose

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
+require("dotenv").config();
 const express = require("express");
-// const mongoose = require("mongoose");
-const app = express();
+const mongoose = require("mongoose");
 const user = require("./routes/user");
+
+const app = express();
+const connectionString = process.env.CONNECTIONSTRING;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/user", user);
+
+try {
+  mongoose.connect(connectionString);
+} catch (err) {
+  process.exit(1);
+}
 
 app.get("/", (_req, res) => {
   res.status(200).json({ message: "Hello World!" });
