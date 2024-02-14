16db82c3e99480999f90a78d3f516d5d807ec7b6
bkmoises <bk_moises@hotmail.com>
2024-02-14 20:09:39 -0300

    chore: inclusão do teste de rotas inesistentes

--- test/app.test.js
+++ test/app.test.js
@@ -9,3 +9,12 @@ it("Deve rodar na porta 3000", () => {
       expect(res.body.message).toBe("Hello World!");
     });
 });
+
+it("Não deve permitir acesso a rota não existente", () => {
+  return request(app)
+    .get("/mockRoute")
+    .then((res) => {
+      expect(res.status).toBe(404);
+      expect(res.body.message).toBe("Rota não encontrada");
+    });
+});
