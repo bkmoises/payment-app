838921efe066221d8fda83b56c365dc5271a34b7
bkmoises <bk_moises@hotmail.com>
2024-02-14 19:59:07 -0300

    refactor: inclusão dos indicadores para garantir que os campos sejam unicos e obrigatórios

--- src/models/users.js
+++ src/models/users.js
@@ -1,11 +1,29 @@
 const mongoose = require("mongoose");
 
-const User = mongoose.Schema({
-  name: String,
-  cpf: String,
-  mail: String,
-  passwd: String,
-  seller: Boolean,
+//  TODO: Implementar uma lógica de criptografia de senha
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  cpf: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mail: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwd: {
+    type: String,
+    required: true,
+  },
+  seller: {
+    type: Boolean,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model("users", User);
+module.exports = mongoose.model("users", userSchema);
