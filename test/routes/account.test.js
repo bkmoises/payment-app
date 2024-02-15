42cc6cbdc035dc37542a27a80acba70519f5b8c9
bkmoises <bk_moises@hotmail.com>
2024-02-15 20:52:05 -0300

    refactor: gerador de cpf movido para um arquivo isolado

--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -3,29 +3,10 @@ const app = require("../../src/app");
 const userDb = require("../../src/models/users");
 const accDb = require("../../src/models/accounts");
 const account = require("../../src/controllers/account");
+const mockCpf = require("../helpers/cpfGenerator");
 
 let user;
 let userId;
-
-const mockCpf = () => {
-  const randomNumber = () => Math.floor(Math.random() * 10);
-
-  const cpfArray = Array.from({ length: 9 }, randomNumber);
-
-  const d1 =
-    cpfArray.reduce((acc, digit, index) => acc + digit * (index + 1), 0) % 11;
-  cpfArray.push(d1 < 10 ? d1 : 0);
-
-  const d2 =
-    cpfArray.reduce((acc, digit, index) => acc + digit * index, 0) % 11;
-  cpfArray.push(d2 < 10 ? d2 : 0);
-
-  const cpf = cpfArray
-    .join("")
-    .replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
-
-  return cpf;
-};
 
 beforeEach(() => {
   user = {
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,28 +1,9 @@
 const request = require("supertest");
 const app = require("../../src/app");
 const db = require("../../src/models/users");
+const mockCpf = require("../helpers/cpfGenerator");
 
 let user;
-
-const mockCpf = () => {
-  const randomNumber = () => Math.floor(Math.random() * 10);
-
-  const cpfArray = Array.from({ length: 9 }, randomNumber);
-
-  const d1 =
-    cpfArray.reduce((acc, digit, index) => acc + digit * (index + 1), 0) % 11;
-  cpfArray.push(d1 < 10 ? d1 : 0);
-
-  const d2 =
-    cpfArray.reduce((acc, digit, index) => acc + digit * index, 0) % 11;
-  cpfArray.push(d2 < 10 ? d2 : 0);
-
-  const cpf = cpfArray
-    .join("")
-    .replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
-
-  return cpf;
-};
 
 beforeEach(() => {
   user = {
