e16812743cbe42eb7908416ca13c6a36a68d138e
bkmoises <bk_moises@hotmail.com>
2024-02-14 21:21:44 -0300

    chore: documentação dos endpoints

--- src/routes/account.js
+++ src/routes/account.js
@@ -1,15 +1,17 @@
 const router = require("express").Router();
 const accountController = require("../controllers/account");
 
+// Rotas para manipulação de contas
 router
   .route("/")
-  .post(accountController.createAccount)
-  .get(accountController.getAllAccounts);
+  .post(accountController.createAccount) // Criar uma nova conta
+  .get(accountController.getAllAccounts); // Obter todas as contas
 
+// Rotas para manipulação de contas por ID
 router
   .route("/:id")
-  .get(accountController.getOneAccount)
-  .put(accountController.updateOneAccount)
-  .delete(accountController.deleteOneAccount);
+  .get(accountController.getOneAccount) // Obter uma conta pelo ID
+  .put(accountController.updateOneAccount) // Atualizar uma conta pelo ID
+  .delete(accountController.deleteOneAccount); // Excluir uma conta pelo ID
 
 module.exports = router;
