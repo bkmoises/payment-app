5b63136fa6b0af6b543eacbe54b1c2f59ebc7b7d
bkmoises <bk_moises@hotmail.com>
2024-02-16 09:23:25 -0300

    chore: inclusão da rota de get para resgatar todas as transações

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const transactionController = require("../controllers/transactionController.js");
 
-router.route("/").post(transactionController.makeTransfer);
+router
+  .route("/")
+  .post(transactionController.makeTransfer)
+  .get(transactionController.getAllTransactions);
 
 module.exports = router;
