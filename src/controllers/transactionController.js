5c6a29aabe6be65fd087cc2b4dec6c96745df7c6
bkmoises <bk_moises@hotmail.com>
2024-02-16 10:58:15 -0300

    chore: inclusão do controller de resgatar transação

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -12,4 +12,9 @@ module.exports = {
       .status(200)
       .json([{ transactionId: 1, payer: 1, payee: 1, value: 0 }]);
   },
+
+  getTransactionById: async (req, res) => {
+    const { id } = req.params;
+    return res.status(200).json({ transactionId: id });
+  },
 };
