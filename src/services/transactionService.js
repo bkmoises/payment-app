0b87fc06fb9729c150c7e07b5820c5579d250fce
bkmoises <bk_moises@hotmail.com>
2024-02-17 20:20:33 -0300

    refactor: melhoria na implementação da interação com banco de dados para tornar o código masi eficiente

--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const dbTrans = require("../models/transaction");
 const dbUser = require("../models/users");
 const dbAccount = require("../models/accounts");
@@ -10,5 +11,53 @@ module.exports = {
       statusCode: 200,
       transactionList,
     };
+  },
+
+  getTransaction: async (id) => {
+    const transaction = await dbTrans.findOne(id);
+
+    return {
+      statusCode: 200,
+      transaction,
+    };
+  },
+
+  revertTransaction: async (id) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+      const transaction = await dbTrans.findOne(id).session(session);
+      const { payer, payee, value } = transaction;
+
+      await dbAccount.updateOne(
+        { userId: payer },
+        { $inc: { balance: value } },
+        { session },
+      );
+      await dbAccount.updateOne(
+        { userId: payee },
+        { $inc: { balance: -value } },
+        { session },
+      );
+
+      await dbTrans.updateOne(id, { reverted: true }, { session });
+
+      await session.commitTransaction();
+      session.endSession();
+
+      return {
+        statusCode: 200,
+        message: "Transação revertida com sucesso",
+      };
+    } catch (error) {
+      await session.abortTransaction();
+      session.endSession();
+
+      return {
+        statusCode: 500,
+        error: "Erro ao reverter a transação",
+      };
+    }
   },
 };
