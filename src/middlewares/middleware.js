275cc20ba978fa3945533a41a5d0368dfc7c7341
bkmoises <bk_moises@hotmail.com>
2024-02-18 15:26:22 -0300

    refactor: early return implementado na função next para evitar else

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -5,11 +5,8 @@ const autorizationVerify = async (_req, res, next) => {
     "https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc",
   );
 
-  if (response.data.message === "Autorizado") {
-    next();
-  } else {
-    return res.status(400).json({ error: "Transação não autorizada" });
-  }
+  if (response.data.message === "Autorizado") return next();
+  return res.status(400).json({ error: "Transação não autorizada" });
 };
 
 module.exports = autorizationVerify;
