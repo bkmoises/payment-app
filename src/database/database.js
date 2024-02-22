93233771e3b6b6d178f38460ce98af80ea94800b
bkmoises <bk_moises@hotmail.com>
2024-02-22 20:17:00 -0300

    chore: inclusão dos métodos para intereção com banco de transação

--- src/database/database.js
+++ src/database/database.js
@@ -1,31 +1,60 @@
-const mongoose = require("mongoose");
 const user = require("../models/users");
 const account = require("../models/accounts");
 const transaction = require("../models/transaction");
 
 module.exports = {
-  // Get all data métods
   findUsers: async () => user.find(),
   findAccounts: async () => account.find(),
   findTransactions: async () => transaction.find(),
 
-  // Create métods
   createUser: async (data) => user.create(data),
   createAccount: async (userId) => account.create({ userId }),
   createTransaction: async (data) => transaction.create(data),
+  revertTransaction: async (id) =>
+    transaction.findByIdAndUpdate(id, { reverted: true }),
 
-  // Find métods
   findUser: async (id) => user.findById(id),
   findAccount: async (id) => account.findById(id),
+  findTransaction: async (id) => transaction.findById(id),
   findAccountByUserId: async (userId) => account.findOne({ userId }),
   findByEmailAndCpf: async (mail, cpf) =>
     user.findOne({ $or: [{ mail }, { cpf }] }),
 
-  // Update métods
   updateUser: async (id, data) => user.findByIdAndUpdate(id, data),
   updateAccount: async (id, data) => account.findByIdAndUpdate(id, data),
 
-  // Delete métods
   deleteUser: async (id) => user.findByIdAndDelete(id),
   deleteAccount: async (id) => account.findByIdAndDelete(id),
+
+  transferMoney: async (payer, payee, value) =>
+    account.bulkWrite([
+      {
+        updateOne: {
+          filter: { userId: payer },
+          update: { $inc: { balance: -value } },
+        },
+      },
+      {
+        updateOne: {
+          filter: { userId: payee },
+          update: { $inc: { balance: value } },
+        },
+      },
+    ]),
+
+  returnValues: async (payer, payee, value) =>
+    account.bulkWrite([
+      {
+        updateOne: {
+          filter: { userId: payer },
+          update: { $inc: { balance: value } },
+        },
+      },
+      {
+        updateOne: {
+          filter: { userId: payee },
+          update: { $inc: { balance: -value } },
+        },
+      },
+    ]),
 };
