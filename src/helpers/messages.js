4a2a509c1272de43486bbd794ad3baa1ccf75dd7
bkmoises <bk_moises@hotmail.com>
2024-02-22 13:52:25 -0300

    chore: inclusão de helper para lidar com mensagens personalizadas

---
+++ src/helpers/messages.js
@@ -0,0 +1,14 @@
+module.exports = {
+  cpfAlreadyExist: "CPF já cadastrado no sistema",
+  emailAlreadyExist: "Email já cadastrado no sistema",
+  successGetUser: "Usuário resgatado com sucesso",
+  successGetUsers: "Usuários resgatados com sucesso",
+  successUpdateUser: "Usuário alterado com sucesso",
+  userNotFound: "Usuário não encontrado",
+  errorToGetUser: "Erro ao resgatar usuário",
+  errorToGetUsers: "Erro ao resgatar usuários",
+  errorToCreateUser: "Erro ao criar usuário",
+  errorToUpdateUser: "Erro ao alterar usuário",
+  errorToDeleteUser: "Erro ao excluir usuário",
+  isRequired: (field) => `O campo ${field} é requerido`,
+};
