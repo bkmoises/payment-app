cf93d0d4c1bde1b07a3945e4f4acf835d38befd9
bkmoises <bk_moises@hotmail.com>
2024-02-22 12:31:21 -0300

    refacotr: remoção dos comentários

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -3,54 +3,43 @@ const accountService = require("../services/accountService");
 module.exports = {
   createAccount: async (req, res) => {
     const { userId } = req.body;
-    // Chama o serviço para criar uma conta
-    const { account, statusCode, error } =
+    const { data, statusCode, error } =
       await accountService.createAccount(userId);
 
-    // Constrói a resposta com base no resultado da criação da conta
-    return res.status(statusCode).json(account || { error });
+    return res.status(statusCode).json(data || { error });
   },
 
   getAllAccounts: async (_req, res) => {
-    // Chama o serviço para obter a lista de todas as contas
-    const { accountList, statusCode, error } =
-      await accountService.getAllAccounts();
+    const { data, statusCode, error } = await accountService.getAllAccounts();
 
-    // Constrói a resposta com base no resultado da criação da conta
-    return res.status(statusCode).json(accountList || { error });
+    return res.status(statusCode).json(data || { error });
   },
 
   getOneAccount: async (req, res) => {
     const { id } = req.params;
-    // Chama o serviço para obter uma conta
-    const { account, statusCode, error } = await accountService.getOneAccount({
+    const { data, statusCode, error } = await accountService.getOneAccount({
       _id: id,
     });
 
-    // Constrói a resposta com base no resultado da criação da conta
-    return res.status(statusCode).json(account || { error });
+    return res.status(statusCode).json(data || { error });
   },
 
   updateOneAccount: async (req, res) => {
     const { id } = req.params;
     const { balance, status } = req.body;
 
-    // Chama o serviço para alterar uma conta
     const { message, statusCode, error } =
       await accountService.updateOneAccount({ _id: id }, { balance, status });
 
-    // Constrói a resposta com base no resultado da criação da conta
-    return res.status(statusCode).json(message ? { message } : { error });
+    return res.status(statusCode).json(error ? { error } : { message });
   },
 
   deleteOneAccount: async (req, res) => {
     const { id } = req.params;
-    // Chama o serviço para remover uma conta
     const { statusCode, error } = await accountService.deleteOneAccount({
       _id: id,
     });
 
-    // Constrói a resposta com base no resultado da criação da conta
     return res.status(statusCode).json({ error });
   },
 };
