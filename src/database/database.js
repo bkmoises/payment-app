0b4361f635278c53ecf16582821fa369072a6fd6
bkmoises <bk_moises@hotmail.com>
2024-02-22 17:08:53 -0300

    chore: inclusão dos métodos de contas

--- src/database/database.js
+++ src/database/database.js
@@ -8,18 +8,24 @@ module.exports = {
   findUsers: async () => user.find(),
   findAccounts: async () => account.find(),
   findTransactions: async () => transaction.find(),
+
   // Create métods
   createUser: async (data) => user.create(data),
-  createAccount: async (data) => account.create(data),
+  createAccount: async (userId) => account.create({ userId }),
   createTransaction: async (data) => transaction.create(data),
+
   // Find métods
   findUser: async (id) => user.findById(id),
+  findAccount: async (id) => account.findById(id),
+  findAccountByUserId: async (userId) => account.findOne({ userId }),
   findByEmailAndCpf: async (mail, cpf) =>
     user.findOne({ $or: [{ mail }, { cpf }] }),
 
   // Update métods
   updateUser: async (id, data) => user.findByIdAndUpdate(id, data),
+  updateAccount: async (id, data) => account.findByIdAndUpdate(id, data),
 
   // Delete métods
   deleteUser: async (id) => user.findByIdAndDelete(id),
+  deleteAccount: async (id) => account.findByIdAndDelete(id),
 };
