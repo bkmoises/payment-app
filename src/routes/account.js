a9cd30a84882ec8bfc7b50c5d61b5de150b4c15c
bkmoises <bk_moises@hotmail.com>
2024-02-23 13:22:42 -0300

    refactor: alterada a declaração do import do controller

--- src/routes/account.js
+++ src/routes/account.js
@@ -1,17 +1,17 @@
 const router = require("express").Router();
-const accountController = require("../controllers/account");
+const controller = require("../controllers/account");
 
 // Rotas para manipulação de contas
 router
   .route("/")
-  .post(accountController.createAccount) // Criar uma nova conta
-  .get(accountController.getAllAccounts); // Obter todas as contas
+  .post(controller.createAccount) // Criar uma nova conta
+  .get(controller.getAllAccounts); // Obter todas as contas
 
 // Rotas para manipulação de contas por ID
 router
   .route("/:id")
-  .get(accountController.getOneAccount) // Obter uma conta pelo ID
-  .put(accountController.updateOneAccount) // Atualizar uma conta pelo ID
-  .delete(accountController.deleteOneAccount); // Excluir uma conta pelo ID
+  .get(controller.getOneAccount) // Obter uma conta pelo ID
+  .put(controller.updateOneAccount) // Atualizar uma conta pelo ID
+  .delete(controller.deleteOneAccount); // Excluir uma conta pelo ID
 
 module.exports = router;
