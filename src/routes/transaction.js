aa497f97cbfe01577490a353d70470fc75beae85
bkmoises <bk_moises@hotmail.com>
2024-02-23 15:02:33 -0300

    refactor: alteração do import da middleware

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const autorizationVerify = require("../middlewares/middleware");
+const authorizationVerify = require("../middlewares/authorization");
 const controller = require("../controllers/transaction");
 
 // Rotas para manipulação de transações
 router
   .route("/")
-  .post(controller.makeTransfer) // Criar uma nova transação
+  .post(authorizationVerify, controller.makeTransfer) // Criar uma nova transação
   .get(controller.getAllTransactions); // Obter todas as transações
 
 // Rotas para manipulação de transações por ID
