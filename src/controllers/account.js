80ba4b8d29af448609621685692359c7a35627d9
bkmoises <bk_moises@hotmail.com>
2024-02-23 13:15:52 -0300

    refactor: alteração do import do arquivo de serviço

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -1,23 +1,22 @@
-const accountService = require("../services/account");
+const service = require("../services/account");
 
 module.exports = {
   createAccount: async (req, res) => {
     const { userId } = req.body;
-    const { data, statusCode, error } =
-      await accountService.createAccount(userId);
+    const { data, statusCode, error } = await service.createAccount(userId);
 
     return res.status(statusCode).json(data || { error });
   },
 
   getAllAccounts: async (_req, res) => {
-    const { data, statusCode, error } = await accountService.getAllAccounts();
+    const { data, statusCode, error } = await service.getAllAccounts();
 
     return res.status(statusCode).json(data || { error });
   },
 
   getOneAccount: async (req, res) => {
     const { id } = req.params;
-    const { data, statusCode, error } = await accountService.getOneAccount(id);
+    const { data, statusCode, error } = await service.getOneAccount(id);
 
     return res.status(statusCode).json(data || { error });
   },
@@ -28,15 +27,17 @@ module.exports = {
 
     const data = { balance, status };
 
-    const { message, statusCode, error } =
-      await accountService.updateOneAccount(id, data);
+    const { message, statusCode, error } = await service.updateOneAccount(
+      id,
+      data,
+    );
 
     return res.status(statusCode).json(error ? { error } : { message });
   },
 
   deleteOneAccount: async (req, res) => {
     const { id } = req.params;
-    const { statusCode, error } = await accountService.deleteOneAccount(id);
+    const { statusCode, error } = await service.deleteOneAccount(id);
 
     return res.status(statusCode).json({ error });
   },
