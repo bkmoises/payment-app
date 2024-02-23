dee111549a6aa4e2230d1ec9bbd2ba57a981ca21
bkmoises <bk_moises@hotmail.com>
2024-02-23 15:02:18 -0300

    refactor: nome alterado

---
+++ src/middlewares/authorization.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const axios = require("axios");
+
+const AUTH_PORT = process.env.AUTH_PORT;
+const URI = `http://localhost:${AUTH_PORT}/authorization`;
+
+const authorizationVerify = async (_req, res, next) => {
+  const response = await axios.get(URI);
+
+  if (response.data.status === "Authorized") return next();
+  return res.status(400).json({ error: "Transação não autorizada" });
+};
+
+module.exports = authorizationVerify;
