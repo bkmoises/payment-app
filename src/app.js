64f9a8fad75c8fea55f7b14969f6f91b9e4c33a3
bkmoises <bk_moises@hotmail.com>
2024-02-28 12:42:03 -0300

    refactor: alteração no nome dos métodos de rotas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const user = require("./routes/user");
-const account = require("./routes/account");
-const transaction = require("./routes/transaction");
+
+const userRouter = require("./routes/user");
+const accountRoutes = require("./routes/account");
+const transactionRoutes = require("./routes/transaction");
 
 const app = express();
 const connectionString = process.env.CONNECTIONSTRING;
@@ -11,16 +12,20 @@ const connectionString = process.env.CONNECTIONSTRING;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/user", user);
-app.use("/account", account);
-app.use("/transaction", transaction);
+// Rotas
+app.use("/user", userRouter);
+app.use("/account", accountRoutes);
+app.use("/transaction", transactionRoutes);
 
+// Conexão com o banco de dados
 mongoose.connect(connectionString);
 
+// Rota padrão
 app.get("/", (_req, res) => {
   res.status(200).json({ message: "Hello World!" });
 });
 
+// Rota para tratamento de rotas não configuradas
 app.all("/*", (_req, res) => {
   res.status(404).json({ message: "Rota não encontrada" });
 });
